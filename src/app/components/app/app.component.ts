e7cfad897fdac64ab6195cc126e17b7a546dc120
Woodie Westbrook <woodie.westbrook@gmail.com>
2016-10-14 00:03:24 -0400

    webpack aint easy

---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import '../../../../public/css/style.css';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'ng2 Application';
+}
+
